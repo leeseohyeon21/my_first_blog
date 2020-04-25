ac56c3d995fd931987fb28416a9fc934dac02297
leeseohyeon <leeseohyeon21@gmail.com>
2020-04-25 15:21:38 +0900

    Changed the HTML for the site

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e454c$q_+b4xsx9(bckt-d769*%-7)pqs%o0v0de+=@mlilhx*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
